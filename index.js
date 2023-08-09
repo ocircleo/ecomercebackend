02e969be36f33dcb609a25bed1f5a307e157554c
ocircleo <salmanhossain11222626@gmail.com>
2023-08-09 20:57:52 +0600

    new database created

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const app = express();
 const cheerio = require("cheerio");
@@ -9,7 +9,6 @@ const port = process.env.port || 1111;
 app.use(cors());
 app.use(express.json());
 require("dotenv").config();
-const tableArray = [];
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ocircleo.zgezjlp.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,7 +22,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    const databse = client.db("phones");
     const phones = client.db("phones").collection("phone");
+    const newPhones = client.db("phones").collection("newPhones");
     app.get("/", async (req, res) => {
       // const url = "https://www.gsmarena.com.bd/xiaomi/";
       res.send("ola");
@@ -76,6 +77,64 @@ async function run() {
     app.post("/addata", async (req, res) => {
       const data = req.body;
       const result = await phones.insertOne(data);
+      res.send(result);
+    });
+    //==== main munctionalitys of the app
+    app.get("/phones/:num", async (req, res) => {
+      const number = parseInt(req.params.num || 0);
+      const query = {};
+      const options = {
+        projection: { _id: 1, Model: 1, Price: 1 },
+      };
+      const result = await phones
+        .find(query, options)
+        .skip(number)
+        .limit(24)
+        .toArray();
+      res.send(result);
+    });
+    // ===== get a single phone =====
+    app.get("/phone/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await phones.findOne({ _id: new ObjectId(id) });
+      console.log(result);
+      res.send(result);
+    });
+    // ===== get data by brand =====
+    app.get("/brand/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const query = { Brand: brand };
+      const option = { projection: { _id: 1, Name: 1 } };
+      const result = await phones.find(query, option).toArray();
+      res.send(result);
+    });
+
+    // =====New=====
+    // =====Database =====
+    //===== adds data to new phone db
+    app.post("/newaddata", async (req, res) => {
+      const data = req.body;
+      const result = await newPhones.insertOne(data);
+      res.send(result);
+    });
+    // ===== finds data by brand from new db =====
+    app.get("/newbrand/:brand", async (req, res) => {
+      const brand = req.params.brand;
+      const query = { brand: brand };
+      const option = { projection: { _id: 1, model: 1, brand: 1 } };
+      const result = await newPhones.find(query, option).toArray();
+      res.send(result);
+    });
+    // ==== gets new data by Id =====
+    app.get("/newphone/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await newPhones.findOne({ _id: new ObjectId(id) });
+      res.send(result);
+    });
+    // ====== deletes new data by id ====
+    app.delete("/deletenewphone/:id", async (req, res) => {
+      const id = req.params.id;
+      const result = await newPhones.deleteOne({ _id: new ObjectId(id) });
       res.send(result);
     });
   } finally {
